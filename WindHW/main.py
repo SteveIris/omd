4775b0595237e4f3c9663a88c6b9bae02f8b065f
SteveIris <sstepaa2@gmail.com>
2022-09-12 15:15:06 +0300

    Поправил нейминг

--- WindHW/main.py
+++ WindHW/main.py
@@ -1,7 +1,7 @@
 import requests
 
-response = requests.get("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
-points = response.json()
-pointNumber=1
-wind = points['dataseries'][pointNumber]['wind10m']
+response = requests.get('https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0')
+data = response.json()
+point_number = 11
+wind = data['dataseries'][point_number]['wind10m']
 print(wind)
