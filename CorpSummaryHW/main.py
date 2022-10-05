ac2049f4c42aac86c1d06e6d759bf7f9bf1b15ff
SteveIris <sstepaa2@gmail.com>
2022-10-05 16:46:07 +0300

    Добавил третью команду

--- CorpSummaryHW/main.py
+++ CorpSummaryHW/main.py
@@ -31,13 +31,52 @@ def first_option():
 
 def second_option():
     """Реализует вторую команду"""
+    departments = create_summary()
+
+    print('Департаменты:')
+    for department in departments:
+        print(f'{department}: ')
+        for dep_property in departments[department]:
+            print(f'    {dep_property}: {departments[department][dep_property]}')
+
+    print('Сохранить отчёт в файл?')
+    option = ''
+    options = {'Да': True, 'Нет': False}
+    while option not in options:
+        print('Выберите команду: {}/{}'.format(*options))
+        option = input()
+    if options[option]:
+        save_as_csv(departments)
+    menu()
+
+
+def third_option():
+    """Реализует третью команду"""
+    save_as_csv(create_summary())
+    menu()
+
+
+def save_as_csv(departments: dict):
+    """Сохраняет в csv-файл сводный отчёт"""
+    with open('Departments_summary.csv', 'w', encoding='utf8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Название', 'Численность', 'Минимальная зп', 'Максимальная зп', 'Средняя зп'])
+        for department in departments:
+            writer.writerow([department]+list(departments[department].values()))
+    print('Отчёт записан в файл Departments_summary.csv')
+    return
+
+
+def create_summary() -> dict:
+    """Создаёт сводный отчёт по департаментам для команд 2 и 3"""
     data = open_file('Corp_Summary.csv')
     departments = {}
 
     for line in data:
         salary = int(line[5])
         if not departments.get(line[1]):
-            departments[line[1]] = {'Численность': 1, 'Минимальная зп': salary, 'Максимальная зп': salary, 'Средняя зп': salary}
+            departments[line[1]] = {'Численность': 1, 'Минимальная зп': salary, 'Максимальная зп': salary,
+                                    'Средняя зп': salary}
         else:
             departments[line[1]]['Численность'] += 1
             departments[line[1]]['Средняя зп'] += salary
@@ -45,19 +84,9 @@ def second_option():
                 departments[line[1]]['Минимальная зп'] = salary
             if int(line[5]) > departments[line[1]]['Максимальная зп']:
                 departments[line[1]]['Максимальная зп'] = salary
-
-    print('Департаменты:')
     for department in departments:
-        print(f'{department}: ')
-        departments[department]['Средняя зп'] = round(departments[department]['Средняя зп']/departments[department]['Численность'], 2)
-        for dep_property in departments[department]:
-            print(f'    {dep_property}: {departments[department][dep_property]}')
-    menu()
-
-
-def third_option():
-    """Будет реализовывать третью команду"""
-    pass
+        departments[department]['Средняя зп'] = round(departments[department]['Средняя зп']/departments[department]['Численность'], 1)
+    return departments
 
 
 def close():
