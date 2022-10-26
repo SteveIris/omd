b0c820ca83d2ea497a0084183add210ada34124a
SteveIris <sstepaa2@gmail.com>
2022-10-26 15:05:18 +0300

    Добавил функцию для тестирования, исправил баг с раскраской

--- AdvertHW/main.py
+++ AdvertHW/main.py
@@ -4,45 +4,57 @@ import json
 class ColorizeMixin:
     """Миксин для раскраски текста"""
 
-    def colorize_text(self, color, text):
+    def colorize_text(self, color, text: str):
         color_str = str(color)
-        return f'\033[1;{color_str};40m{text} '
+        return f'\033[1;{color_str};40m{text} \033[0;0;0m'
 
 
 class AdvertWithoutConditions:
     """Преобразует словарь в python-объект с доступом к атрибутам через точку. Не накладывает ограничений на данные"""
-    def __init__(self, advertisement):
+    def __init__(self, advertisement: dict):
 
         for attribute in advertisement:
             if isinstance(advertisement[attribute], dict):
                 setattr(self, attribute, AdvertWithoutConditions(advertisement[attribute]))
             else:
                 setattr(self, attribute, advertisement[attribute])
+        return
 
 
 class Advert(ColorizeMixin, AdvertWithoutConditions):
     """Преобразует словарь в python-объект с доступом к атрибутам через точку. Требует price>=0 и наличия title"""
     repr_color_code = 33
 
-    def __init__(self, advertisement):
+    def __init__(self, advertisement: dict):
+        self.price = 0
         AdvertWithoutConditions.__init__(self, advertisement)
         self.check_correctness()
+        return
 
     def check_correctness(self):
         """Проверка на то, что введён title и price>=0"""
         if not hasattr(self, 'title'):
             print("Error: must have title")
             exit(0)
-        if hasattr(self, 'price'):
-            if self.price < 0:
-                print("Error: price is negative")
-                exit(0)
-        else:
-            self.price = 0
+        if self.price < 0:
+            print("Error: price is negative")
+            exit(0)
+        return
 
     def __repr__(self):
         """Выводит название и цену из объявления"""
         return self.colorize_text(Advert.repr_color_code, f' {self.title} | {self.price} ₽')
+
+
+def test(test_str: str):
+    """Переводит строку с объявлением в dict а затем в Advert-объект и выводит его параметры"""
+    test_dict = json.loads(test_str)
+    test_ad = Advert(test_dict)
+    print(test_ad.title)
+    print(test_ad.price)
+    print(test_ad.location.address)
+    print(test_ad)
+    return
 
 
 def main():
@@ -54,12 +66,7 @@ def main():
     "metro_stations": ["Белорусская"]
     }
     }"""
-    lesson = json.loads(lesson_str)
-    lesson_ad = Advert(lesson)
-    print(lesson_ad.title)
-    print(lesson_ad.price)
-    print(lesson_ad.location.address)
-    print(lesson_ad)
+    test(lesson_str)
     korgi_str = """{
     "title": "Вельш-корги",
     "price": 1000,
@@ -68,12 +75,16 @@ def main():
     "address": "сельское поселение Ельдигинское, поселок санатория Тишково, 25"
     }
     }"""
-    korgi = json.loads(korgi_str)
-    korgi_ad = Advert(korgi)
-    print(korgi_ad.title)
-    print(korgi_ad.price)
-    print(korgi_ad.location.address)
-    print(korgi_ad)
+    test(korgi_str)
+    no_price_str = """{
+        "title": ":)",
+        "price": -2,
+        "class": "emoji",
+        "location": {
+        "address": "Таганрог"
+        }
+        }"""
+    test(no_price_str)
 
 
 if __name__ == '__main__':
