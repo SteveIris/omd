ff4044c34dd64aa79aa9fe65155863df6472e8a2
SteveIris <sstepaa2@gmail.com>
2022-12-02 17:40:27 +0300

    Добавил классы для ДЗ к занятию 6

--- CountVectorizerHW/main.py
+++ CountVectorizerHW/main.py
@@ -1,3 +1,4 @@
+import math
 from collections import defaultdict
 
 
@@ -23,6 +24,47 @@ class CountVectorizer:
         return [[self.matrix[word][number] for word in self.matrix.keys()] for number in range(len(corpus))]
 
 
+def tf_transform(count_matrix: list) -> list:
+    """Принимает терм-документную матрицу и возвращает матрицу повторений"""
+    sums = [sum(document) for document in count_matrix]
+    tf_matrix = [[round(i/total, 3) for i in document] for document, total in zip(count_matrix, sums)]
+    return tf_matrix
+
+
+def idf_transform(count_matrix: list) -> list:
+    """Принимает терм-документную матрицу и возвращает матрицу обратных частот"""
+    total_docs = len(count_matrix)
+    transposed_matrix = list(map(list, zip(*count_matrix)))
+    docs_with_word = [sum(map(bool, word)) for word in transposed_matrix]
+    return [round(1+math.log((total_docs+1)/(amount+1)), 1) for amount in docs_with_word]
+
+
+class TfidfTransformer:
+
+    def __init__(self):
+        """Инициализирует пустые матрицы"""
+        self.tf_matrix = []
+        self.idf_matrix = []
+
+    def fit_transform(self, count_matrix: list) -> list:
+        """Принимает терм-документную матрицу и возвращает матрицу tf-idf"""
+        self.tf_matrix = tf_transform(count_matrix)
+        self.idf_matrix = idf_transform(count_matrix)
+        return [[round(a*b, 3) for a, b in zip(tf_doc, self.idf_matrix)] for tf_doc in self.tf_matrix]
+
+
+class TfidfVectorizer(CountVectorizer):
+
+    def __init__(self):
+        """Инициализирует родительский класс и экземпляр Transsformer"""
+        super().__init__()
+        self.transformer = TfidfTransformer()
+
+    def fit_transform(self, corpus: list) -> list:
+        """Принимает список строк и возвращает соответствующую tf-idf матрицу"""
+        return self.transformer.fit_transform(super().fit_transform(corpus))
+
+
 def main():
     """Здесь тестируется созданный класс"""
     corpus1 = [
@@ -36,15 +78,29 @@ def main():
         'Не каждый охотник фазан'
     ]
     vectorizer = CountVectorizer()
-    assert vectorizer.fit_transform(corpus1) == [[1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-                                                 [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
+    transformer = TfidfTransformer()
+    tf_idf_vectorizer = TfidfVectorizer()
+    count_matrix = [
+        [1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+    ]
+    assert vectorizer.fit_transform(corpus1) == count_matrix
     assert vectorizer.get_feature_names() == ['crock', 'pot', 'pasta', 'never', 'boil', 'again', 'pomodoro',
                                               'fresh', 'ingredients', 'parmesan', 'to', 'taste']
+    assert tf_transform(count_matrix) == [[0.143, 0.143, 0.286, 0.143, 0.143, 0.143, 0, 0, 0, 0, 0, 0],
+                                                          [0, 0, 0.143, 0, 0, 0, 0.143, 0.143, 0.143, 0.143, 0.143, 0.143]]
+    assert idf_transform(count_matrix) == [1.4, 1.4, 1.0, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4]
+    assert transformer.fit_transform(count_matrix) == [[0.2, 0.2, 0.286, 0.2, 0.2, 0.2, 0, 0, 0, 0, 0, 0],
+                                                       [0, 0, 0.143, 0, 0, 0, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]]
     assert vectorizer.fit_transform(corpus2) == [[1, 1, 1, 1, 1, 1, 1, 0],
                                                  [1, 1, 1, 1, 1, 1, 1, 0],
                                                  [0, 0, 0, 0, 1, 0, 0, 0],
                                                  [1, 1, 0, 0, 0, 0, 1, 1]]
     assert vectorizer.get_feature_names() == ['каждый', 'охотник', 'желает', 'знать', 'где', 'сидит', 'фазан', 'не']
+    assert tf_idf_vectorizer.fit_transform(corpus1) == [[0.2, 0.2, 0.286, 0.2, 0.2, 0.2, 0, 0, 0, 0, 0, 0],
+                                                        [0, 0, 0.143, 0, 0, 0, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]]
+    assert tf_idf_vectorizer.get_feature_names() == ['crock', 'pot', 'pasta', 'never', 'boil', 'again', 'pomodoro',
+                                                     'fresh', 'ingredients', 'parmesan', 'to', 'taste']
     print('Всё работает')
 
 
